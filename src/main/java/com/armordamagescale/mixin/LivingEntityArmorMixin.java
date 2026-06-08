f9a7ad99984459befd8bb2cc5ad96d25033e723e
someaddons <someaddons@github.com>
2026-06-08 21:25:15 +0200

    3.4: Allow damage reduction to zero damage instead of maxing out at 0.5

--- src/main/java/com/armordamagescale/mixin/LivingEntityArmorMixin.java
+++ src/main/java/com/armordamagescale/mixin/LivingEntityArmorMixin.java
@@ -112,7 +112,7 @@ public abstract class LivingEntityArmorMixin extends Entity
                     log += " Toughnessvalue:" + toughness + " dmg after toughness reduction:" + modamage;
                 }
             }
-            cir.setReturnValue(Math.max(0.5f, modamage));
+            cir.setReturnValue(Math.max(0.0f, modamage));
 
             if (ArmorDamage.config.getCommonConfig().debugprint && !log.isEmpty())
             {
